a442e54d71da9d4761259f5fde4932402d128f65
saad naikwade <naikwadesaad@gmail.com>
2025-09-18 09:19:41 +0530

    some responsive

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// dark mode toggle
 const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+  document.body.classList.toggle("dark", checkbox.checked);
+});
+
+// hamburger togglee
+const hamburger = document.querySelector(".hamburger");
+const navcon = document.querySelector(".navcon");
+
+hamburger.addEventListener("click", () => {
+  navcon.classList.toggle("active");
 });
